0ad68dbf0406cd2c278ad832c61fa868ded2f9be
Mariano Longo <marianolongoort@outlook.com>
2020-03-28 01:13:32 -0300

    Agregado de MediaQueries y modificaciones de navegación. Propuesta de modificación generica de footer.

--- footer.js
+++ footer.js
@@ -2,7 +2,7 @@ document.write(`
 <hr >
 <footer class="footer mt-auto py-3">
   <div class="container">
-    <span class="text-muted">© 2020 - Federico Marchese - Html basics.</span>
+    <span class="text-muted">© 2020 - ORT - Programación en nuevas tecnologías 1 - Sede Belgrano - Html basics.</span>
   </div>
 </footer>
 `);
