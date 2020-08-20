c1ecae0f713dc2a3a8da26734f550dd535c344a9
federico-marchese-zenfolio <fmarchese@zenfolio.com>
2020-08-19 21:53:00 -0300

    corrección de navbar para live server y mac

--- navbar.js
+++ navbar.js
@@ -1,14 +1,15 @@
 //Fines academicos
-//var path = window.location.pathname;
-//var dir = path.substring(1, path.lastIndexOf('/')) + '/';
-//var pisoRoot = dir.lastIndexOf('html-intro-master');
-//var techo = dir.indexOf('/', pisoRoot);
-//var pathRoot = dir.substring(0, techo);
+var path = window.location.pathname;
+var dir = path.substring(navigator.platform == 'Win32' ? 1 : 0, path.lastIndexOf('/')) + '/';
+var pisoRoot = dir.lastIndexOf('html-intro');
+var techo = dir.indexOf('/', pisoRoot);
+// si se utiliza live server el root es vacío.
+var pathRoot = pisoRoot != -1 ? dir.substring(0, techo) : "";
 
 document.write(`
 <header style="height:85px;">
         <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-            <a class="navbar-brand" href="../index.html">Html Basics</a>
+            <a class="navbar-brand" href="${pathRoot}/index.html">Html Basics</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
                 aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -16,19 +17,19 @@ document.write(`
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="/1-html/index.html">Html</a>
+                        <a class="nav-link" href="${pathRoot}/1-html/index.html">Html</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/2-css/index.html">Css</a>
+                        <a class="nav-link" href="${pathRoot}/2-css/index.html">Css</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/3-js/index.html">Javascript</a>
+                        <a class="nav-link" href="${pathRoot}/3-js/index.html">Javascript</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/4-ejemplos/index.html">Ejemplos</a>
+                        <a class="nav-link" href="${pathRoot}/4-ejemplos/index.html">Ejemplos</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/5-ejercicios/index.html">Ejercicios</a>
+                        <a class="nav-link" href="${pathRoot}/5-ejercicios/index.html">Ejercicios</a>
                     </li>
                 </ul>
             </div>
